be2c948c4c4d7a49159c1d2e743e2e166a637d6c
Abidouul <ammor-2011@hotmail.com>
2026-06-04 04:33:12 +0200

    Deploy game to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? '/car-drift/' : '/',
   server: {
     watch: {
       ignored: ['**/.context/**', '**/dist/**', '**/node_modules/**'],
